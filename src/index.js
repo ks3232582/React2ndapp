f2bc6b15bc718955975bd12afe6524c9415e3aa1
Kundan <ks3232582@gmail.com>
2024-08-06 12:56:34 +0530

    my first app in react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css'
+
+
+let curDate = new Date();
+curDate=curDate.getHours();
+ let greeting='';
+ const cssStyle={
+ }
+
+if(curDate >=1 && curDate <12){
+greeting='Good Morning';
+cssStyle.color='green'
+}else if(curDate >=12 && curDate < 17)
+{
+  greeting='Good Afternoon'
+  cssStyle.color='Orange'
+}else{
+  greeting='Good Night'
+  cssStyle.color='black'
+}
+
+
+ReactDOM.render(
+<h1>Hello Sir,<span style={cssStyle}> {greeting} </span></h1>,document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
